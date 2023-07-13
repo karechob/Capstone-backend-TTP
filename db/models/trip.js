0a38eea28f2f938a67a2fb04af919c3176f23ac7
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-13 16:05:32 -0400

    feat[database models]: add values to trip model inside trip.js

--- db/models/trip.js
+++ db/models/trip.js
@@ -2,20 +2,40 @@ const { DataTypes } = require("sequelize");
 const db = require("../db");
 
 const Trip = db.define("trip", {
-    Trips_Id: {
+    id: {
+        primaryKey: true,
         type: DataTypes.INTEGER,
         allowNull: false
     },
-    Trip_Budget: {
+    origin: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    destination: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    budget: {
         type: DataTypes.DECIMAL(10,2),
         allowNull: false
     },
-    Trip_Activities: {
+    isCurrent: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    weather: {
         type: DataTypes.STRING,
         allowNull: true
+    },
+    duration: {
+        type: DataTypes.INTEGER,
+        allowNull: true
     }
-
-    
 });
 
 module.exports = Trip;

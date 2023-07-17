351c8e52520f085a2c5102368ef390c68045d234
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-16 23:41:55 -0400

    fix[router]: fix problem with router that didn't allow a get request from pulling data

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-router.use('/user', require('./user'));
+router.use("/users", require("./user"));
 
 router.use((req, res, next) => {
-    const error = new Error("404 Not Found");
-    error.status = 404;
-    next(error);
+  const error = new Error("404 Not Found");
+  error.status = 404;
+  next(error);
 });
 
 module.exports = router;

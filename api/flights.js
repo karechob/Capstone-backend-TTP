a9f965747de4decc15e479fd73ce55b7e000bf2d
eluzung <luzungelaine@gmail.com>
2023-07-17 11:44:19 -0400

    feat: Add myFlights route
    Fetch data of all flights associated with trips that are owned by a specific user.
    
    feat: Add new association where User owns a single trip

--- api/flights.js
+++ api/flights.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { Flight } = require("../db/models");
+//const { where } = require("sequelize");
+const { Flight, User, Trip } = require("../db/models");
 
 //fetch all flights
 router.get("/", async (req, res, next) => {
@@ -13,6 +14,28 @@ router.get("/", async (req, res, next) => {
     console.log(error);
     next(error);
   }
+});
+
+router.get("/myFlights", async (req, res, next) => {
+    try {
+      // All trips for current User where they are the owner
+      const trips = await Trip.findAll({where: { ownerId: 1 }})
+      let flights = [];
+      //console.log(trips.length);
+
+      for(let i = 0; i < trips.length; i++) {
+        //console.log(trips[i]);
+        const currentFlight = await Flight.findOne({where: { tripId: trips[i].dataValues.id}})
+        flights.push(currentFlight);
+      }
+
+      console.log("flights array: ", flights);
+      res.json(flights);
+      //const flights = await Flight.findAll({where: {tripId : }})
+      //console.log()
+    } catch (error) {
+      next(error);
+    }
 });
 
 module.exports = router;

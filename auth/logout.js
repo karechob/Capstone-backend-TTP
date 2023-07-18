f2e998e8d0d4eff9e338cf22a782aae95e6599d9
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-18 15:33:50 -0400

    feat[logout-auth]: create logout-auth-logic

---
+++ auth/logout.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+router.post("/", (req, res, next) => {
+  const user = req.user.dataValues.username;
+  req.logout((error) => {
+    if (error) {
+      return next(error);
+    }
+    req.session.destroy((error) => {
+      if (error) {
+        console.error("Error occurred during logout:", error);
+        return res
+          .status(500)
+          .json({ error: "An error occurred during logout" });
+      }
+      console.log(`${user} logged out successfully`);
+      res.status(200).json({ message: "Logged out successfully" });
+    });
+  });
+});
+
+module.exports = router;

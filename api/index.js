ff599746aba5d140f1af0292a2f7e83a6d71e92f
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-18 15:47:19 -0400

    feat[api-routes]: create api routes in index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
+const isAuthorized = require("../middleware/authMiddleware");
+const isAdmin = require("../middleware/adminMiddleware");
+router.use("/users", isAdmin, require("./user"));
 
-router.use("/users", require("./user"));
+router.use("/trips", isAuthorized, require("./trips"));
 
-router.use("/trips", require("./trips"));
+router.use("/hotels", isAuthorized, require("./hotels"));
 
-router.use("/hotels", require("./hotels"));
+router.use("/flights", isAuthorized, require("./flights"));
 
-router.use("/flights", require("./flights"));
+router.use("/activities", isAuthorized, require("./activities"));
 
-router.use("/activities", require("./activities"));
+router.use("/profile", isAuthorized, require("./profile"));
 
 router.use((req, res, next) => {
   const error = new Error("404 Not Found");

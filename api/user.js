20abb2a81ea420fd6c2f139b66fa8ab4478e36f7
naa-7 <nalawad000@citymail.cuny.edu>
2023-07-18 15:48:22 -0400

    edit[user routes]: edit routes of user in api

--- api/user.js
+++ api/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const { User } = require("../db/models");
+const { Flight, User, Trip } = require("../db/models");
 
-//fetch all Users
+// Fetch all users
 router.get("/", async (req, res, next) => {
   try {
     const allUsers = await User.findAll({ attributes: ["id", "email"] });
@@ -15,4 +15,38 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+// Fetch user by ID
+router.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId);
+
+    user ? res.status(200).json(user) : res.status(404).send("User Not Found");
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/:id/details", async (req, res, next) => {
+  try {
+    const ownerId = req.params.id;
+    console.log(ownerId);
+    const trips = await Trip.findAll({ where: { ownerId } });
+    if (!trips || trips.length === 0) {
+      return res.status(404).json({ message: "No trips found for the user" });
+    }
+    const tripIds = trips.map((trip) => trip.id);
+    const flights = await Flight.findAll({ where: { tripId: tripIds } });
+
+    const responseData = {
+      trips: trips,
+      flights: flights,
+    };
+
+    res.json(responseData);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;

3ccb090b583e52fabc84b481057703928fba85d1
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-25 09:24:51 -0400

    feat[hotels]: create hotels api call file

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 router.use("/me", require("./user"));
 router.use("/admin", require("./admin"));
 router.use("/flights", require("./flights"))
+router.use("/hotels", require("./hotels"))
 
 router.use((req, res, next) => {
   const error = new Error("404 Not Found");

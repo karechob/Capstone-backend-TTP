c2cd258cf9e41b451bdf41bd3455b180da6a24ae
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-25 12:34:32 -0400

    feat&refactor[return & departure]: add return api endpoint for airport

--- api/flights.js
+++ api/flights.js
@@ -2,12 +2,51 @@ const router = require("express").Router();
 const axios = require('axios')
 require('dotenv').config();
 
-router.get('/airport', async function (req, res, next) {
+router.get('/departure', async function (req, res, next) {
   try {
     const options = {
       method: 'GET',
       url: 'https://priceline-com-provider.p.rapidapi.com/v1/flights/locations',
-      params: {name: 'Venice Italy'}, //required (sample location)
+      params: {name: 'Dubai'}, //required (sample location)
+      headers: {
+        'X-RapidAPI-Key':  process.env.X_FLIGHT_API_KEY,
+        'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+      }
+    };
+    //lat (doesnt exist if its 0)
+    //long (doesnt exist if its 0)
+
+    const response = await axios.request(options);
+    const allAirports = response.data;
+    const airportData = [];
+
+    for (const airport of allAirports) {
+      if(airport.lat && airport.lon !== 0)
+      airportData.push({
+        itemName: airport.itemName,
+        id: airport.id,
+        stateCode: airport.stateCode,
+        countryCode: airport.countryCode,
+        cityName: airport.cityName,
+        provinceName: airport.provinceName,
+        entered: airport.entered,
+        lat: airport.lat ? airport.lat : 0,
+        lon: airport.lon ? airport.lon : 0
+      });
+    }
+    
+    res.json(airportData)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.get('/return', async function (req, res, next) {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://priceline-com-provider.p.rapidapi.com/v1/flights/locations',
+      params: {name: 'New York'}, //required (sample location)
       headers: {
         'X-RapidAPI-Key':  process.env.X_FLIGHT_API_KEY,
         'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
@@ -48,11 +87,11 @@ router.get('/allflights', async function (req, res, next) {
       method: 'GET',
       url: 'https://priceline-com-provider.p.rapidapi.com/v2/flight/roundTrip',
       params: {
-        departure_date: '2023-12-21,2023-12-25', //need this value from user
+        departure_date: '2023-08-01,2023-08-09', //need this value from user
         adults: '1', //maximum 8 //need this value from user
         sid: 'iSiX639',
-        origin_airport_code: 'YWG,JFK', //get this value through /airport --> airport.id
-        destination_airport_code: 'JFK,YWG', //get this value through /airport  --> airport.id
+        origin_airport_code: 'JFK,DXB', //get this value through /airport --> airport.id
+        destination_airport_code: 'DXB,JFK', //get this value through /airport  --> airport.id
         number_of_itineraries: '4',
         currency: 'USD'
       },

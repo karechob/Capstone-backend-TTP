7b2df0f48c47ce6eca30a1bd306c046ac29cf1c1
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-25 07:54:26 -0400

    feat[api/flights]: add itinerary details

--- api/flights.js
+++ api/flights.js
@@ -2,46 +2,62 @@ const router = require("express").Router();
 const axios = require('axios')
 require('dotenv').config();
 
-router.get('/allflights', async function(req, res, next) {
-    try {
-        const options = {
-            method: 'GET',
-            url: 'https://priceline-com-provider.p.rapidapi.com/v2/flight/roundTrip',
-            params: {
-              departure_date: '2023-12-21,2023-12-25',
-              adults: '1',
-              sid: 'iSiX639',
-              origin_airport_code: 'YWG,JFK',
-              destination_airport_code: 'JFK,YWG',
-              number_of_itineraries: '4'
-            },
-            headers: {
-              'X-RapidAPI-Key': process.env.X_FLIGHT_API_KEY,
-              'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
-            }
-          };
-          const response = await axios.request(options);
-          const allFlightsResult = response.data.getAirFlightRoundTrip.results.result;
-          const baselineTotalFare = allFlightsResult.itinerary_data;
-          const prices = [];
+router.get('/allflights', async function (req, res, next) {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://priceline-com-provider.p.rapidapi.com/v2/flight/roundTrip',
+      params: {
+        departure_date: '2023-12-21,2023-12-25',
+        adults: '1',
+        sid: 'iSiX639',
+        origin_airport_code: 'YWG,JFK',
+        destination_airport_code: 'JFK,YWG',
+        number_of_itineraries: '4'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.X_FLIGHT_API_KEY,
+        'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
+      }
+    };
+    const response = await axios.request(options);
+    const allFlightsResult = response.data.getAirFlightRoundTrip.results.result;
+    const baselineTotalFare = allFlightsResult.itinerary_data;
+    const prices = [];
 
-          for (const key in baselineTotalFare){
-              const pricebaseline = baselineTotalFare[key]
-            if(baselineTotalFare.hasOwnProperty(key)){
-              console.log(`${key} : ${JSON.stringify(pricebaseline.slice_data)}`)
-              prices.push(`${key} : ${pricebaseline.price_details.baseline_total_fare}`)
-            }
-          }
+    for (let i = 0; i < 4; i++) {
+      const key = `itinerary_${i}`;
+      if (baselineTotalFare.hasOwnProperty(key)) {
+        const sliceData = baselineTotalFare[key].slice_data.slice_0;
+        const airline = sliceData.airline;
+        const departure = sliceData.departure;
+        const arrival = sliceData.arrival;
 
+        const priceDetails = baselineTotalFare[key].price_details.baseline_total_fare;
 
-          
-          res.json(baselineTotalFare);
-    } catch (error) {
-        console.log(error)
+        prices.push({
+          itinerary: key,
+          airline: airline,
+          departure: departure,
+          arrival: arrival,
+          price: priceDetails
+        });
+      }
     }
-}) 
+
+    res.json(prices);
+  } catch (error) {
+    console.log(error)
+  }
+})
 //baseline_total_fare
 
+// [
+//   "itinerary_0 : 470.01",
+//   "itinerary_1 : 470.53",
+//   "itinerary_2 : 481.32",
+//   "itinerary_3 : 481.32"
+// ]
 
 
 module.exports = router;

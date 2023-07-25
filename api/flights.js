7bdf38411dfea42bb6b7e962d1c2a6408f7866be
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-25 10:53:10 -0400

    feat[flights]: add links for airlines

--- api/flights.js
+++ api/flights.js
@@ -7,7 +7,7 @@ router.get('/airport', async function (req, res, next) {
     const options = {
       method: 'GET',
       url: 'https://priceline-com-provider.p.rapidapi.com/v1/flights/locations',
-      params: {name: 'Venice Italy'}, //required
+      params: {name: 'Venice Italy'}, //required (sample location)
       headers: {
         'X-RapidAPI-Key':  process.env.X_FLIGHT_API_KEY,
         'X-RapidAPI-Host': 'priceline-com-provider.p.rapidapi.com'
@@ -51,8 +51,8 @@ router.get('/allflights', async function (req, res, next) {
         departure_date: '2023-12-21,2023-12-25', //need this value from user
         adults: '1', //maximum 8 //need this value from user
         sid: 'iSiX639',
-        origin_airport_code: 'BKK,JFK', //get this value through /airport --> airport.id
-        destination_airport_code: 'JFK,BKK', //get this value through /airport  --> airport.id
+        origin_airport_code: 'YWG,JFK', //get this value through /airport --> airport.id
+        destination_airport_code: 'JFK,YWG', //get this value through /airport  --> airport.id
         number_of_itineraries: '4',
         currency: 'USD'
       },
@@ -64,7 +64,25 @@ router.get('/allflights', async function (req, res, next) {
     const response = await axios.request(options);
     const allFlightsResult = response.data.getAirFlightRoundTrip.results.result;
     const itinararies = allFlightsResult.itinerary_data;
+    const airlineLink = allFlightsResult.airline_data
     const flightInformation = [];
+
+    for(let i = 0; i < 4; i++) {
+      const key = `airline_${i}`
+      if (airlineLink.hasOwnProperty(key)) {
+        const sliceData = airlineLink[key];
+        const name = sliceData.name;
+        const website = sliceData.websiteUrl;
+        const phone = sliceData.phoneNumber;
+        if (website && phone !== '') {
+          flightInformation.push({
+            name: name,
+            website: website,
+            phone: phone,
+          })
+        }
+      }
+    }
 
     for (let i = 0; i < 4; i++) {
       const key = `itinerary_${i}`;
@@ -73,7 +91,6 @@ router.get('/allflights', async function (req, res, next) {
         const airline = sliceData.airline;
         const departure = sliceData.departure;
         const arrival = sliceData.arrival;
-
         const priceDetails = itinararies[key].price_details.baseline_total_fare;
 
         flightInformation.push({
--- api/hotels.js
+++ api/hotels.js
@@ -2,4 +2,44 @@ const router = require("express").Router();
 const axios = require('axios')
 require('dotenv').config();
 
+router.get('/', async function (req, res, next) {
+    try {
+        const options = {
+            method: "GET",
+            url: "https://booking-com.p.rapidapi.com/v2/hotels/search",
+            params: {
+                order_by: "popularity",
+                adults_number: "2",
+                checkin_date: "2023-09-27",
+                filter_by_currency: "AED",
+                dest_id: "-553173",
+                locale: "en-gb",
+                checkout_date: "2023-09-28",
+                units: "metric",
+                room_number: "1",
+                dest_type: "city",
+                include_adjacency: "true",
+                children_number: "0",
+                page_number: "0",
+                children_ages: "5,0",
+                categories_filter_ids: "class::2,class::4,free_cancellation::1",
+            },
+            headers: {
+                "X-RapidAPI-Key": process.env.X_HOTEL_API_KEY,
+                "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
+            },
+        };
+
+        const response = await axios.request(options);
+        const allHotels = response.data;
+
+        res.json(allHotels)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
+
 module.exports = router;

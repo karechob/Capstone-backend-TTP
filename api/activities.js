ce26d7e32851fa6583537c74507bd81b93c64b57
eluzung <luzungelaine@gmail.com>
2023-07-27 20:12:51 -0400

    fix(activities): Add additional Places API requests
    The response data now provides places that are tourist attractions and food places in one array.

--- api/activities.js
+++ api/activities.js
@@ -2,25 +2,57 @@ const router = require("express").Router();
 const axios = require("axios");
 require("dotenv").config();
 
-router.get("/allactivities", async function (req, res, next) {
-  //const destination = req.trip.destination;
-  //if (destination) {
+router.post("/allactivities", async (req, res, next) => {
   try {
-    const textSearch = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=chicago&radius=50000&key=${process.env.PLACES_API_KEY}`
-    );
+    const resultsArray = [];
 
+    // Takes in a destination param provided from the frontend to request a single place from that location
+    var textSearchOptions = {
+      method: "get",
+      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.body.destination}&radius=50000&key=${process.env.PLACES_API_KEY}`,
+      headers: {},
+    };
+
+    const textSearch = await axios.request(textSearchOptions);
+
+    // The response data from the previous call gives a latitude and longitude that is needed for the next api calls
     let latitude = textSearch.data.results[0].geometry.location.lat;
     let longitude = textSearch.data.results[0].geometry.location.lng;
 
-    const nearbySearch = await axios.get(
-      ` https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=50000&type=restaurant&key=${process.env.PLACES_API_KEY}`
-    );
-    res.json(nearbySearch.data);
+    // Calling the nearbySearch request will return a list of places in the area provided by the latitude and longitude. Limited to only return tourist attractions or related places.
+    var tourist_attraction = {
+      method: "get",
+      url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=1500&type=tourist_attraction&key=${process.env.PLACES_API_KEY}`,
+      headers: {},
+    };
+
+    const tourist_attraction_Search = axios.request(tourist_attraction);
+
+    // Another nearbySearch request with the type specified as restaurants or food places.
+    var food = {
+      method: "get",
+      url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude}%2C${longitude}&radius=1500&type=restaurant&key=${process.env.PLACES_API_KEY}`,
+      headers: {},
+    };
+
+    const food_Search = axios.request(food);
+
+    // Using a Promise.allSettled() method to handle the multiple axios requests. 
+    const results = await Promise.allSettled([
+      tourist_attraction_Search,
+      food_Search,
+    ]);
+
+    // Pushing the necessary response data items from the results to a resultsArray
+    results.map((item, index) => {
+      resultsArray.push(item.value.data.results);
+    });
+
+    //console.log(resultsArray);
+    //res.json(resultsArray);
   } catch (error) {
-    console.error();
+    console.error(error);
   }
-  //}
 });
 
 module.exports = router;

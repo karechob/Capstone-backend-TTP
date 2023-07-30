83327a973e254af8cb5f98e5861bde590bf35da6
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-30 18:31:09 -0400

    fix: run seed js

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ require("pg");
 // const db = new Sequelize(`postgres://localhost:5432/${name}`, {
 //     logging: false,
 // });
-const db = new Sequelize(
+const db = process.env.NODE_ENV == "dev" ?  new Sequelize(
   process.env.POSTGRES_DATABASE,
   process.env.POSTGRES_USER,
   process.env.POSTGRES_PASSWORD,
@@ -15,11 +15,9 @@ const db = new Sequelize(
       "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
     logging: false,
   }
+) :  new Sequelize (
+  `${process.env.POSTGRES_URL}?sslmode=require`
 );
 
-//for seeding production db
-// const db = new Sequelize (
-//   "postgres://default:HD7ALUskqBN8@ep-shy-voice-12880499-pooler.us-east-1.postgres.vercel-storage.com/verceldb?sslmode=require"
-// )
 
 module.exports = db;

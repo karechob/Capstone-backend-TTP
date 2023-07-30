71ed4cec7ba7c9076e0c57609117a53e67c426e8
Miranda Karecho <109288708+karechob@users.noreply.github.com>
2023-07-30 16:03:43 -0400

    feat[index.js]: add handler for serverless function

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ require("dotenv").config();
 const PORT = 8080;
 const sessionStore = new SequelizeStore({ db });
 
-express.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-
-
 // Helper functions
 const serializeUser = (user, done) => done(null, user.id);
 const deserializeUser = async (id, done) => {
@@ -80,6 +75,15 @@ const setupPassport = () => {
 const setupRoutes = (app) => {
   app.use("/api", require("./api"));
   app.use("/auth", require("./auth"));
+};
+
+exports.handler = async (req, res) => {
+
+  if (req.method === "GET" && req.url === "/") {
+    res.status(200).send("Express on Vercel");
+  } else {
+    res.status(404).send("Not Found");
+  }
 };
 
 // Start server and sync the db
